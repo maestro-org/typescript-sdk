970f2509840fdca37d58ac9e93c09120917ef676
SIDANWhatever <wongkahinhinson@gmail.com>
2023-10-07 15:27:35 +0800

    docs: updating docs for accountsAddresses

--- src/api/accounts/type.ts
+++ src/api/accounts/type.ts
@@ -1,10 +1,20 @@
 /**
  * Query parameters for fetching account addresses.
- * @param {number | null} [count] - The max number of results per page.
- * @param {string | null} [cursor] - Pagination cursor string.
- *    Use the cursor included in a page of results to fetch the next page.
+ * @export
+ * @interface AccountHistory
+ *
  */
 export interface AccountAddressesQueryParams {
+    /**
+     * The max number of results per page.
+     * @type {number | null}
+     * @memberof AccountAddressesQueryParams
+     */
     count?: number | null;
+    /**
+     * Pagination cursor string, use the cursor included in a page of results to fetch the next page.
+     * @type {string | null}
+     * @memberof AccountAddressesQueryParams
+     */
     cursor?: string | null;
 }
